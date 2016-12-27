ea1f27286f6965c17b2afac97d08ec7b580fa7f4
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2016-12-27 16:55:54 +0300

    Autoboxing pooling

--- intro/src/main/java/ru/interface1/courses/java/advanced/Autoboxing.java
+++ intro/src/main/java/ru/interface1/courses/java/advanced/Autoboxing.java
@@ -2,6 +2,22 @@ package ru.interface1.courses.java.advanced;
 
 public class Autoboxing {
     public static void main(String[] args) {
+//        test();
+
+        poolingTest();
+    }
+
+    private static void poolingTest() {
+        Integer i1 = 10;
+        Integer i2 = 10;
+        System.out.println(i1 == i2);
+
+        i1 = 128;
+        i2 = 128;
+        System.out.println(i1 == i2);
+    }
+
+    private static void test() {
         Integer j = 171; // создание объекта+упаковка
         Integer k = ++j; // распаковка+операция+упаковка
         System.out.println(j == k);
@@ -9,7 +25,5 @@ public class Autoboxing {
         k += i + j;
 
         System.out.println(k);
-
-
     }
 }
