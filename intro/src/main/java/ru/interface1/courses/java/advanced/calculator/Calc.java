efcb16242fb45cc9a6916279a84a10dbbf2b04ee
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2016-12-29 10:29:49 +0300

    Stub

---
+++ intro/src/main/java/ru/interface1/courses/java/advanced/calculator/Calc.java
@@ -0,0 +1,7 @@
+package ru.interface1.courses.java.advanced.calculator;
+
+public class Calc {
+    public static void main(String[] args) {
+        // TODO: 29/12/2016 Реализовать вычисление
+    }
+}
