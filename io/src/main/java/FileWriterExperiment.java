9b795b50ddc9786374615c77dae8add91f1b346e
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2016-12-30 16:02:55 +0300

    Java 7 - style file writing

--- io/src/main/java/FileWriterExperiment.java
+++ io/src/main/java/FileWriterExperiment.java
@@ -8,24 +8,12 @@ public class FileWriterExperiment {
 
     @SneakyThrows
     public static void main(String[] args) {
-        // JavaSE6 style
-        PrintWriter pw = null;
-        try {
-            // создание потокового объекта (открытие потока)
-            FileWriter out = new FileWriter("text.txt");
-
-            // придание потоковому объекту требуемых свойств
-            BufferedWriter br = new BufferedWriter(out);
-
-            pw = new PrintWriter(br);
-
+        // JavaSE7 style
+        try (PrintWriter pw = new PrintWriter(
+                new BufferedWriter(
+                        new FileWriter("text.txt")))) {
             // работа с потоком через потоковый объект
             pw.println("I'm a sentence in a text-file.");
-        } finally {
-            if (pw != null) {
-                pw.close(); // закрытие потока
-            }
         }
-
     }
 }
